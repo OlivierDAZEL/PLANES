aeb0672c603593c7de1ed31acb8187c186747d86
Olivier DAZEL <olivier.dazel@univ-lemans.fr>
2016-02-20 08:28:58 +0100

    with anise properties

--- src/Materials/Mat_porous_10.m
+++ src/Materials/Mat_porous_10.m
@@ -46,9 +46,19 @@ rho_1=126.000;
 young=694400E+00;
 nu=0.24000E+00;
 eta=0.05;
-angle_x=pi/4;
+angle_x=0*pi/2;
 angle_y=0;
 angle_z=0;
+sig_x=sig;
+sig_y=sig;
+sig_z=sig;
+alpha_x=alpha;
+alpha_y=alpha;
+alpha_z=alpha;
+LCV_x=LCV;
+LCV_y=LCV;
+LCV_z=LCV;
+
 
 N=(young)/(2*(1+nu));
 A_hat=(young*nu)/((1+nu)*(1-2*nu));
@@ -57,9 +67,9 @@ F_hat=1*A_hat;
 C_hat_conservative= [A_hat+2*N A_hat F_hat 0 0 0;A_hat A_hat+2*N F_hat 0 0 0;F_hat F_hat A_hat+2*N 0 0 0; 0 0 0 N 0 0;0 0 0 0 N 0; 0 0 0 0 0 N ];
 C_tot_0=[1 1 1 0 0 0;1 1 1 0 0 0;1 1 1 0 0 0;0 0 0 0 0 0;0 0 0 0 0 0;0 0 0 0 0 0];
 
-sig_tensor=diag([sig 10*sig sig]);
-alpha_tensor=diag([alpha alpha alpha]);
-LCV_tensor=diag([LCV LCV LCV]);
+sig_tensor=diag([sig_x sig_y sig_z]);
+alpha_tensor=diag([alpha_x alpha_y alpha_z]);
+LCV_tensor=diag([LCV_x LCV_y LCV_z]);
 
 alpha_hat=0.33348;
 beta_hat =812.69e3;
